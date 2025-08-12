d241d4c0e126170adb51bde215b5256c23453fc7
Idayat Sanni <idayatsanni@gmail.com>
2025-08-12 19:08:27 -0400

    change file styling

---
+++ interact.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded", () => {
+  let form = document.getElementById("submit");
+
+  form.addEventListener("submit", validate());
+
+  let bookTitle = document.getElementById("title");
+  let bookAuthor = document.getElementById("author");
+  
+});
