6f9558d950b54e982be554cfd6691d9c11304d78
Idayat Sanni <idayatsanni@gmail.com>
2025-08-23 18:53:33 -0400

    display error notification

--- interact.js
+++ interact.js
@@ -3,8 +3,6 @@ window.addEventListener("DOMContentLoaded", () => {
 
   const isRequired = (value) => (value === "" ? false : true);
 
-  const isBetween = (length, min, max) =>
-    length < min || length > max ? false : true;
   const isEmailValid = (email) => {
     const re =
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -18,13 +16,45 @@ window.addEventListener("DOMContentLoaded", () => {
     return re.test(password);
   };
 
-  // function errorNotifications(text) {
-  //   var displayValidationText = document.querySelector(".display-notification");
-  //   displayValidationText.classList.remove("success");
-  //   displayValidationText.classList.add("error");
+  function errorMessage(input) {
+    const inputFieldContainer = input.parentElement;
 
-  //   displayValidationText.innerHTML = "You have to input a message";
-  // }
+    inputFieldContainer.classList.remove("success");
+    inputFieldContainer.classList.add("error");
+    input.focus;
+    const errorField = inputFieldContainer.querySelector(
+      ".display-notification"
+    );
+    console.log(errorField);
+    const convertTouppercase = input.name;
+    let name = convertTouppercase.toUpperCase();
+    errorField.textContent = `The ${name} Cannot be blank`;
+  }
+
+  function successMessage(input) {
+    const inputFieldContainer = input.parentElement;
+
+    inputFieldContainer.classList.remove("error");
+    inputFieldContainer.classList.add("success");
+
+    const errorField = inputFieldContainer.querySelector(
+      ".display-notification"
+    );
+    console.log(errorField);
+    errorField.textContent = "";
+  }
+
+  function validateTextInput(text) {
+    console.log(text);
+    const valid = true;
+    const textInput = text.value;
+    if (textInput === "" && textInput === null) {
+      successMessage(text);
+      return valid;
+    } else {
+      errorMessage(text);
+    }
+  }
 
   // function validateInput(text) {
   //   if (!isRequired(text) && !isNotNull(text)) {
@@ -32,25 +62,30 @@ window.addEventListener("DOMContentLoaded", () => {
   // }
 
   form.addEventListener("submit", (e) => {
-    alert("clicked");
-    // bookGenre.forEach((element) => {
-    //   console.log(element.checked);
-    // });
+    let bookTitle = document.getElementById("title");
+    const validateBook = validateTextInput(bookTitle);
+    let bookAuthor = document.getElementById("author");
 
-    let bookTitle = document.getElementById("title").value;
-    let bookAuthor = document.getElementById("author").value;
-    let date = document.getElementById("date").value;
-    let rating = document.getElementById("rating").value;
+    const validateAuthor = validateTextInput(bookAuthor);
+    let date = document.getElementById("date");
+
+    const validateDate = validateTextInput(date);
+    let rating = document.getElementById("rating");
+    const validateRating = validateTextInput(rating);
     let bookGenre = document.getElementsByName("genre");
+
     let rereadSection = document.getElementsByName("re-read");
 
-    let review = document.getElementById("review").value;
-    console.log(bookAuthor);
-    console.log(date);
-    console.log(rating);
-    console.log(typeof bookGenre);
-    console.log(typeof rereadSection);
-    console.log(bookTitle);
+    let review = document.getElementById("review");
+    const validateReview = validateTextInput(review);
+
+    const isFormValid =
+      validateBook &&
+      validateAuthor &&
+      validateDate &&
+      validateRating &&
+      validateReview;
+
     e.preventDefault();
   });
 });
